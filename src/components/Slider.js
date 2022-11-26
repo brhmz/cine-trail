77cdddaa8c3b4959d2e7fe03433108e37703b2dd
brhmz <ibrahimoz.mail@gmail.com>
2022-11-26 01:26:06 +0100

    Slider buttons are finished.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import  '../styles/slider.css';
 import {Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { MdOutlineKeyboardArrowLeft } from 'react-icons/md';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 function Slider({movie}) {
 
+  const [upComingMovies, setUpComingMovies] = useState([])
+  const [i, setI] = useState(0)
 
-
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=4fb47f1613fc6ff82ac98a531050f625')
+      .then(response => setUpComingMovies(response.data.results))
+      .catch(err => console.log(err))
+  })
 
   const imageBaseUrl = 'https://image.tmdb.org/t/p/original'
+  const upComingMoviesIds = upComingMovies.map(item=>item.id)
 
-  // console.log(upComingMovies)
+const next = () => { i<(upComingMovies.length-1) && i>=0 ? setI(i+1) : setI(0)}
+const previous = () => { i>0 && i<=(upComingMovies.length-1) ? setI(i-1) : setI(upComingMovies.length-1)}
+
+
 
 
   return (
-    <div className='slider-component'>
-    <img className='slider-image' src={imageBaseUrl+movie.backdrop_path}  />
+    <div className='slider-component' style={{ backgroundImage:`url(${imageBaseUrl+upComingMovies[i]?.backdrop_path})`}}>
+    <div className='slider-overlay'></div>
+    <MdOutlineKeyboardArrowRight className='right-arrow' onClick={()=>next()}/>
+    <MdOutlineKeyboardArrowLeft className='left-arrow' onClick={()=>previous()}/>
+    
       
   
         
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import Slider from '../components/Slider';
 import { ThemeContext } from '../contexts/ThemeContext';
 import '../styles/homepage.css'
@@ -9,25 +9,12 @@ function HomePage() {
 
   const {darkMode, setDarkMode} = useContext(ThemeContext)
 
-  const [upComingMovies, setUpComingMovies] = useState([])
 
-  useEffect(() => {
-    axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=4fb47f1613fc6ff82ac98a531050f625')
-      .then(response => setUpComingMovies(response.data.results))
-      .catch(err => console.log(err))
-  })
 
   return (
     <div className={darkMode===true ? 'home-page home-page-dark' : 'home-page'}>
       <div className='slider-container'>
-        {
-          upComingMovies.map((item, id) => {
-            return <Slider 
-              movie={item}
-              key={id}
-            />        
-          })
-        }
+       <Slider /> 
       </div>
       
     </div>
