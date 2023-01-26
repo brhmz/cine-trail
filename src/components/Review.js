ca986943ca26d74158d1c64f5c49e0f5ac83eb92
brhmz <ibrahimoz.mail@gmail.com>
2023-01-26 14:40:41 +0100

    Made some corrections.

--- src/components/Review.js
+++ src/components/Review.js
@@ -2,16 +2,20 @@ import React, {useContext, useEffect, useState} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import ReactReadMoreReadLess from "react-read-more-read-less";
 import '../styles/moviedetailpage.css'
+import avatar from '../assests/squirtle.jpg'
 
 function Review({review}) {
 
   const apiKey = process.env.REACT_APP_API_KEY
   const { darkMode } = useContext(ThemeContext)
+  const [imageError, setImageError] = useState(false)
 
   return (
       <div className={darkMode === true ? 'review review-dark' : 'review'}>
         <div className="author-container">
-          <img src="" alt="" className="author-avatar" />
+          <img src={imageError ? avatar : `https://image.tmdb.org/t/p/original/${review?.author_details?.avatar_path}`} alt={review?.author.name} 
+          className="author-avatar" 
+          onError={() => setImageError(true)}/>
           <p className="author-name">{review?.author}</p>
         </div>
         <div className="review-content">
--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, {useEffect, useState, useContext, useMemo } from 'react'
+import React, { useEffect, useState, useContext, useMemo } from 'react'
 import { useParams } from 'react-router';
 import { ThemeContext } from '../contexts/ThemeContext';
 import '../styles/moviedetailpage.css'
@@ -15,7 +15,7 @@ function MovieDetailPage() {
   const apiKey = process.env.REACT_APP_API_KEY
   const { darkMode } = useContext(ThemeContext)
 
-  const { movieId } = useParams();  
+  const { movieId } = useParams();
   const [selectedMovie, setSelectedMovie] = useState(null)
   const [trailerUrl, setTrailerUrl] = useState(null)
   const [isFavorite, setFavorite] = useState(false)
@@ -29,7 +29,7 @@ function MovieDetailPage() {
       setnumberOfReviewsShown(numberOfReviewsShown + 3);
     } else if (numberOfReviewsShown >= reviews.length) {
       setnumberOfReviewsShown(3);
-    } 
+    }
   };
 
   const itemsToShow = useMemo(() => {
@@ -43,63 +43,65 @@ function MovieDetailPage() {
 
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}&language=en-US`)
-      .then(response=>setTrailerUrl(response.data.results.find((item) => (item.type) === "Trailer")))
-      .catch(err=>console.log(err))
+      .then(response => setTrailerUrl(response.data.results.find((item) => (item.type) === "Trailer")))
+      .catch(err => console.log(err))
 
-      axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
-      .then(response=>setSelectedMovie(response.data))
-      .catch(err=>console.log(err))
+    axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`)
+      .then(response => setSelectedMovie(response.data))
+      .catch(err => console.log(err))
 
-      axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${apiKey}&language=en-US`)
-      .then(response=>setReviews(response.data.results))
-      .catch(err=>console.log(err))
-   }, [movieId, apiKey])
+    axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${apiKey}&language=en-US`)
+      .then(response => setReviews(response.data.results))
+      .catch(err => console.log(err))
+  }, [movieId, apiKey])
 
   return (
-    <div className={darkMode===true ? 'movie-detail-page-container movie-detail-page-container-dark' : 'movie-detail-page-container'}>
+    <div className={darkMode === true ? 'movie-detail-page-container movie-detail-page-container-dark' : 'movie-detail-page-container'}>
       <div className='trailer-container'>
         <ReactPlayer
-            className='react-player'
-            url={videoBaseUrl + trailerUrl?.key}
-            width='100%'
-            height='100%'
-          />
+          className='react-player'
+          url={videoBaseUrl + trailerUrl?.key}
+          width='100%'
+          height='100%'
+        />
       </div>
       <div className='movie-details-container'>
         <div className="movie-title">
           <h2>{selectedMovie?.title}</h2>
         </div>
-      <div className='movie-rating'>
-        <RatingStars
+        <div className='movie-rating'>
+          <RatingStars
             currentRating={selectedMovie?.vote_average}
-        />
-        <p className={isFavorite === false ? "is-favorite-false" : "is-favorite-true"} onClick={()=>setFavorite(!isFavorite)}>{isFavorite === false ? "Add to favorites." : "Remove from favorties."}</p>
-      </div>
-      <div className='movie-details'>
-          <img className="movie-detail-image" src={imageBaseUrl + selectedMovie?.poster_path} alt="" />
-        <div className="movie-info">
-          <h2>{selectedMovie?.tagline}</h2>
-          <h4>{selectedMovie?.overview}</h4>
-          <h4>Status: {selectedMovie?.status}</h4>
-          <h4>Runtime: {selectedMovie?.runtime}</h4>
-          <h4>Budget: {selectedMovie?.budget}</h4>
-          <h4>
-          <Genres
-            currentMovie={selectedMovie}
           />
-          </h4>
+          <p className={isFavorite === false ? "is-favorite-false" : "is-favorite-true"} onClick={() => setFavorite(!isFavorite)}>{isFavorite === false ? "Add to favorites." : "Remove from favorties."}</p>
+        </div>
+        <div className='movie-details'>
+          <div className="movie-detail-image-container">
+            <img className="movie-detail-image" src={imageBaseUrl + selectedMovie?.poster_path} alt="" />
+          </div>
+          <div className="movie-info">
+            <h2>{selectedMovie?.tagline}</h2>
+            <h4>{selectedMovie?.overview}</h4>
+            <h4>Status: {selectedMovie?.status}</h4>
+            <h4>Runtime: {selectedMovie?.runtime}</h4>
+            <h4>Budget: {selectedMovie?.budget}</h4>
+            <h4>
+              <Genres
+                currentMovie={selectedMovie}
+              />
+            </h4>
+          </div>
+        </div>
+        <div>
+          <h2>Reviews</h2>
+          <hr />
+          <div>
+            {itemsToShow.length ? itemsToShow : "There is no comment yet."}
+          </div>
+          <button id={reviews?.length <= 3 ? "no-show-more-button" : "show-more-button"} onClick={showMore}>{numberOfReviewsShown >= reviews?.length ? 'End of reviews. Collapse!' : 'Read more reviews'}</button>
+          <hr />
         </div>
       </div>
-      <div>
-        <h2>Reviews</h2>
-        <hr />
-        <div>
-        {itemsToShow.length ? itemsToShow : "There is no comment yet."}
-        </div> 
-        <button id='show-more-button' onClick={showMore}>{numberOfReviewsShown >= reviews?.length ? 'End of reviews. Collapse!' : 'Read more reviews'}</button> 
-        <hr />
-      </div>
-    </div>
     </div>
   )
 }
