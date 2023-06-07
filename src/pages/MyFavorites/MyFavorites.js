6d7ba55d44aa7b1d71955e1bb2d4e1bb00881823
ibrahim öz <ibrahimoz.mail@gmail.com>
2023-06-07 13:27:52 +0200

    My Favorites page created.

--- src/pages/MyFavorites/MyFavorites.js
+++ src/pages/MyFavorites/MyFavorites.js
@@ -4,40 +4,44 @@ import { UserContext } from '../../contexts/UserContext';
 import './myfavorites.css'
 import axios from 'axios';
 import MovieCard from '../../components/MovieCard/MovieCard';
+import headerImg from '../../assests/cinema-stuff-red.jpg'
 
 function MyFavorites({ serverUrl }) {
 
     const { darkMode } = useContext(ThemeContext)
     const { user, token } = useContext(UserContext)
-    const { favoriteMovies, setFavoriteMovies } = useState([])
+    const [ favoriteMovies, setFavoriteMovies ] = useState([])
 
     useEffect(() => {
         axios.get(`${serverUrl}/favoriteMovies/user/${user?._id}`)
-            .then(res => { console.log(res.data) })
+            .then(res => { setFavoriteMovies(res.data.favorites) })
             .catch(err => console.log(err))
-    }, [user])
+    }, [])
 
     return (
         <div className={darkMode === true
             ? 'my-favorites-page-container my-favorites-page-container-dark'
             : 'my-favorites-page-container'}>
-            <h3>My Favorite Movies</h3>
+            <img className={token ? 'logged-in-header' : 'logged-out-header'}
+            src={headerImg}/>
+            <h3 className='my-favorites-title'>My Favorite Movies</h3>
             <div>
                 {
                     token
-                        ? <p>Girdin {user?._id}</p>
-                        
-                        /* favoriteMovies.map(item => {
+                        ? <div className='favorite-movies-container' >
+                        {
+                           favoriteMovies?.map(item => {
                             return <MovieCard
                                 cardStyle={"popular-movie"}
-                                radius={"16px"}
                                 width={"200px"}
                                 height={"300px"}
                                 imageUrl={item?.movie[0].poster_path}
                                 key={item?.movie[0]._id}
                                 data={item?.movie[0]}
-                            />
-                        }) */
+                            /> }) 
+                        }
+                             
+                        </div>  
                         : <p>Please Sign In to add favorite movies.</p>
                 }
             </div>
